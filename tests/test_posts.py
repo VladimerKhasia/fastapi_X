f06e562bc5449d9784d7402e9643a514112a2fd9
Vladimer Khasia <56228503+VladimerKhasia@users.noreply.github.com>
2024-05-12 13:19:43 +0400

    alembic

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -4,20 +4,20 @@ from fastapi.encoders import jsonable_encoder
 
 ### ---------------------- post request tests
 
-# @pytest.mark.parametrize("title, content, published", [
-#     ("title_1", "content_1", True),
-#     ("title_2", "content_2", False),
-# ])
-# def test_create_one_post(logged_client, test_user, title, content, published):
-#     post_data = { "title": title, "content": content, "published": published}
-#     re = logged_client.post("/posts/", json=post_data)
-#     new_post = schemas.Post(**re.json())
+@pytest.mark.parametrize("title, content, published", [
+    ("title_1", "content_1", True),
+    ("title_2", "content_2", False),
+])
+def test_create_one_post(logged_client, test_user, title, content, published):
+    post_data = { "title": title, "content": content, "published": published}
+    re = logged_client.post("/posts/", json=post_data)
+    new_post = schemas.Post(**re.json())
 
-#     assert re.status_code == 201
-#     assert new_post.title == title
-#     assert new_post.content == content
-#     assert new_post.published == published
-#     assert new_post.owner_id == test_user['id']
+    assert re.status_code == 201
+    assert new_post.title == title
+    assert new_post.content == content
+    assert new_post.published == published
+    assert new_post.owner_id == test_user['id']
 
 def test_unauthorized_create_post(client):
     post_data = { "title": "unauth_title", "content": "unauth_content", "published": True}
@@ -26,36 +26,36 @@ def test_unauthorized_create_post(client):
 
 ### ---------------------- get request tests
 
-# def test_get_all_posts(logged_client, test_posts):
-#     re = logged_client.get("/posts/")
-#     # print(re.json()[0]["Post"])
-#     # print(f"\n\n------------------\n\n{jsonable_encoder(test_posts)[0]}")
-#     def response_validator(response_post):
-#         response_post = schemas.PostResponse(**response_post) 
-#         return response_post.Post.model_dump() 
-#     def test_post_validator(test_post):
-#         test_post = schemas.Post(**jsonable_encoder(test_post))
-#         return test_post.model_dump() 
+def test_get_all_posts(logged_client, test_posts):
+    re = logged_client.get("/posts/")
+    # print(re.json()[0]["Post"])
+    # print(f"\n\n------------------\n\n{jsonable_encoder(test_posts)[0]}")
+    def response_validator(response_post):
+        response_post = schemas.PostResponse(**response_post) 
+        return response_post.Post.model_dump() 
+    def test_post_validator(test_post):
+        test_post = schemas.Post(**jsonable_encoder(test_post))
+        return test_post.model_dump() 
     
-#     re_posts = list(map(response_validator, re.json())) 
-#     tst_posts = list(map(test_post_validator, test_posts))
+    re_posts = list(map(response_validator, re.json())) 
+    tst_posts = list(map(test_post_validator, test_posts))
 
-#     re_posts = sorted(re_posts, key=lambda x: sorted(x.items()))
-#     tst_posts = sorted(tst_posts, key=lambda x: sorted(x.items()))
-#     assert re.status_code == 200
-#     assert re_posts == tst_posts
+    re_posts = sorted(re_posts, key=lambda x: sorted(x.items()))
+    tst_posts = sorted(tst_posts, key=lambda x: sorted(x.items()))
+    assert re.status_code == 200
+    assert re_posts == tst_posts
 
 def test_unauthorized_get_all_posts(client, test_posts):
     re = client.get("/posts/")
     assert re.status_code == 401
 
-# def test_get_one_post(logged_client, test_posts):
-#     re = logged_client.get(f"/posts/{test_posts[0].id}")
-#     one_post = schemas.PostResponse(**re.json())
-#     assert re.status_code == 200
-#     assert one_post.Post.id == test_posts[0].id
-#     assert one_post.Post.title == test_posts[0].title
-#     assert one_post.Post.content == test_posts[0].content
+def test_get_one_post(logged_client, test_posts):
+    re = logged_client.get(f"/posts/{test_posts[0].id}")
+    one_post = schemas.PostResponse(**re.json())
+    assert re.status_code == 200
+    assert one_post.Post.id == test_posts[0].id
+    assert one_post.Post.title == test_posts[0].title
+    assert one_post.Post.content == test_posts[0].content
 
 def test_unauthorized_get_one_post(client, test_posts):
     re = client.get(f"/posts/{test_posts[0].id}")
