42e6f47b0eeba6a93cceab950cac57a7184d85f1
Vladimer Khasia <56228503+VladimerKhasia@users.noreply.github.com>
2024-05-12 17:27:44 +0400

    set utc in my github-actions workflow

--- app/oauth2.py
+++ app/oauth2.py
@@ -13,12 +13,9 @@ SECRET_KEY = config.settings.SECRET_KEY
 ALGORITHM = config.settings.ALGORITHM
 ACCESS_TOKEN_EXPIRE_MINUTES = config.settings.ACCESS_TOKEN_EXPIRE_MINUTES
 
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
+def create_access_token(data: dict):
     to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt    
